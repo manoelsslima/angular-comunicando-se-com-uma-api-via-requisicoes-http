926c8238c9b23803602ef737fe63d62727853b31
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-12-22 17:11:39 -0400

    feat: add MensagemErroComponent with template, styles, and tests

---
+++ indexa/src/app/componentes/mensagem-erro/mensagem-erro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensagem-erro',
+  standalone: true,
+  imports: [],
+  templateUrl: './mensagem-erro.component.html',
+  styleUrl: './mensagem-erro.component.css'
+})
+export class MensagemErroComponent {
+
+}
